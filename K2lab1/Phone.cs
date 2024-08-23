9e6dafed18b474aa1b79535bbb4f749831c4c493
lvkax <sasha.livak22@gmail.com>
2024-08-23 12:44:56 +0300

    Виділив клас в іншому файлі

---
+++ K2lab1/Phone.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace K2lab1
+{
+    internal class Phone
+    {
+        public string Name;
+        public string Model;
+        public string ReleaseYear;
+        public int YearOfPurchase;
+        public string BatteryCapacity;
+        public string ProcessorName;
+        public string AmountOfCores;
+        public string AmountOfRAM;
+        public string AmountOfMemory;
+        public string Diagonal;
+        public bool Has3Cameras;
+        public bool HasWirelessCharging;
+
+        public double getGaranteEndingYear()
+        {
+            if (Name == "iPhone") return YearOfPurchase + 1;
+            else if (Name == "Samsung") return YearOfPurchase + 2;
+            else return YearOfPurchase + 3;
+        }
+    }
+}
--- K2lab1/Program.cs
+++ K2lab1/Program.cs
@@ -6,28 +6,7 @@ using System.Threading.Tasks;
 
 namespace K2lab1
 {
-    class Phone
-    {
-        public string Name;
-        public string Model;
-        public string ReleaseYear;
-        public int YearOfPurchase;
-        public string BatteryCapacity;
-        public string ProcessorName;
-        public string AmountOfCores;
-        public string AmountOfRAM;
-        public string AmountOfMemory;
-        public string Diagonal;
-        public bool Has3Cameras;
-        public bool HasWirelessCharging;
-
-        public double getGaranteEndingYear()
-        {
-            if (Name == "iPhone") return YearOfPurchase + 1;
-            else if (Name == "Samsung") return YearOfPurchase + 2;
-            else return YearOfPurchase + 3;
-        }
-    }
+    
     internal class Program
     {
         static void Main(string[] args)
